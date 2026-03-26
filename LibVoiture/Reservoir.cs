aba199b97f0c3498a278f45a09be4bf9425f2e76
kewennenumericsio-crypto <kewenne.numeric.sio@gmail.com>
2026-03-26 17:33:23 +0100

    partie2

---
+++ LibVoiture/Reservoir.cs
@@ -0,0 +1,37 @@
+﻿namespace LibVoiture
+{
+    public class Reservoir
+    {
+        private int capacite;
+        private int contenu;
+
+        public Reservoir(int capacite)
+        {
+            this.capacite = capacite;
+            this.contenu = 0;
+        }
+        public Reservoir() { }
+
+        public int GetCapacite() => this.capacite;
+        public int GetContenu() => this.contenu;
+
+        public void Vider()
+        {
+            this.contenu = 0;
+        }
+
+        public void Remplir(int quantite)
+        {
+            // ensure we don't exceed capacity
+            if (quantite < 0) return;
+            this.contenu = Math.Min(quantite, this.capacite);
+        }
+
+        public override string ToString()
+        {
+            return $"Reservoir [capacite={this.capacite}, contenu={this.contenu}]";
+        }
+    }
+
+}
+
---
+++ Réservoire/Program.cs
@@ -0,0 +1,6 @@
+﻿using LibVoiture;
+            // Exemple d'utilisation (voir la consigne sur l'image)
+            var r = new Reservoir(50);
+            r.Remplir(30);
+            Console.WriteLine(r.ToString());
+       
